bcd8eb383cf68f3d806454402d443727a2418a22
Sampathaddukapu <sampath.addukapu@gmail.com>
2024-02-23 11:21:18 +0530

    new demo2 file added e

--- demo.tf
+++ demo.tf
@@ -6,5 +6,11 @@ resource "aws_instance" "web" {
   tags = {
     Name = random_pet.name.id
   }
-}
-The arguments 
+}esource2 "aws_instance" "web" {
+  ami                    = "ami-a0cfeed8"
+  instance_type          = "t2.micro"
+  user_data              = file("init-script.sh")
+
+  tags = {
+    Name = random_pet.name.id
+  }
